c8e03048cdc232fbd8adc2376ba75dee4f3d0ad1
techxensolutions <asmarnaeem36@gmail.com>
2025-02-03 22:23:29 +0500

    add

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,17 @@
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
   images: {
     domains: ["res.cloudinary.com"],
+  },
+  async redirects() {
+    return [
+      {
+        source: "/en",
+        has: [{ type: "host", value: "www.komneuf.com" }],
+        destination: "https://www.komneuf.ma/en",
+        permanent: true,
+      },
+    ];
   },
 };
 
